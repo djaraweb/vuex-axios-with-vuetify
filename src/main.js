d92dc81b462ec3a55c68ad23e2f0b4283cf0f3d1
deyvi <djara@gmail.com>
2020-03-25 18:16:34 -0500

    [Plantilla Base usando Vuetify]

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "font-awesome/css/font-awesome.css";
 
-Vue.config.productionTip = false
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App)
+}).$mount("#app");
